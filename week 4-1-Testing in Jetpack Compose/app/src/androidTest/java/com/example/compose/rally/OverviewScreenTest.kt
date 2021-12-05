00fedda1a71be170e02034d388592560f2ca773c
hello2727 <jihye7369@gmail.com>
2021-12-06 01:16:26 +0900

    코드랩 4주차-1 codelab 완료 - Testing in Jetpack Compose

---
+++ week 4-1-Testing in Jetpack Compose/app/src/androidTest/java/com/example/compose/rally/OverviewScreenTest.kt
@@ -0,0 +1,28 @@
+package com.example.compose.rally
+
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import com.example.compose.rally.ui.overview.OverviewBody
+import org.junit.Rule
+import org.junit.Test
+
+/**
+ * Created by Jihye Noh
+ * Date: 2021-12-06
+ */
+class OverviewScreenTest {
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun overviewScreen_alertsDisplayed() {
+        composeTestRule.setContent {
+            OverviewBody()
+        }
+
+        composeTestRule
+            .onNodeWithText("Alerts")
+            .assertIsDisplayed()
+    }
+}
